034f9c07334a0ba12a7b5766096c7e74a81c1d77
limalices <alicelimasantos@gmail.com>
2022-03-31 22:10:03 -0300

    adicionei proprietario e fiz mudanças no código

--- casa_completa.py
+++ casa_completa.py
@@ -1,30 +1,43 @@
-'''
-Tarefa: criar mobília (pegar de casa_quarto_mobilia_com_LR.py) e colocar espelho no banheiro
-'''
-
 from config import *
+
+class Proprietario(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(254))
+    email = db.Column(db.String(254))
+    telefone = db.Column(db.String(254))
+
+    casas = db.relationship("Casa", backref = "proprietario")
+
+    def __str__(self) -> str:
+        return f'proprietário(a): ({self.id}) {self.nome}, {self.email}, {self.telefone}'
 
 class Casa(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     formato = db.Column(db.String(254))
 
-    quartos = db.relationship("Quarto", backref = "casa")
+    comodos = db.relationship("Comodo", backref = "casa")
+
+    proprietario_id = db.Column(db.Integer, db.ForeignKey(Proprietario.id), nullable = False)
 
     def __str__(self) -> str:
-        return f'Casa: {self.formato}'
+        s = f'casa {self.formato}'
+        if self.proprietario:
+            s += f' é do(a) {str(self.proprietario)}'
+        return s
 
-class Quarto(db.Model):
+class Comodo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(254))
     dimensoes = db.Column(db.String(254))
 
     casa_id = db.Column(db.Integer, db.ForeignKey(Casa.id), nullable = False)
 
-    mobilias = db.relationship("Mobilia", backref = "quarto")
+    mobilias = db.relationship("Mobilia", backref = "comodo")
 
     def __str__(self) -> str:
-        s = f'Quarto: {self.nome}, {self.dimensoes}, em: {str(self.casa)}'
-        s += f' na casa: {str(self.casa)}'          
+        s = f' cômodo: {self.nome}, {self.dimensoes}'
+        #if self.casa:
+        s += f' na {str(self.casa)}'
         return s
 
 class Mobilia(db.Model):
@@ -33,12 +46,12 @@ class Mobilia(db.Model):
     funcao = db.Column(db.String(254))
     material = db.Column(db.String(254))
 
-    quarto_id = db.Column(db.Integer, db.ForeignKey(Quarto.id), nullable = True)
+    quarto_id = db.Column(db.Integer, db.ForeignKey(Comodo.id), nullable = True)
 
     def __str__(self) -> str:
         s = f'Mobília: ({self.id}) {self.nome}, {self.funcao}, {self.material}'
-        if self.quarto:
-            s += f', localizada em: {str(self.quarto)}'
+        if self.comodo:
+            s += f', no {str(self.comodo)}'
         return s
 
 if __name__ == "__main__":
@@ -51,68 +64,45 @@ if __name__ == "__main__":
     print("*** TESTE criando objetos")
     print()
 
-    c1 = Casa(formato = "Russa")
+    p1 = Proprietario(nome = "Alice", email = "alice@gmail.com", telefone = "123")
+    db.session.add(p1)
+    db.session.commit()
+    print(p1)
+    print()
+
+    c1 = Casa(formato = "russa", proprietario = p1)
     db.session.add(c1)
     db.session.commit()
-    print(c1)
-    print()
+    
+    q1 = Comodo(nome = "sala", dimensoes = "6x5 metros", casa = c1)
+    q2 = Comodo(nome="banheiro", dimensoes="3x4 metros", casa = c1)
 
-    q1 = Quarto(nome = "Sala", dimensoes = "6x5 metros", casa = c1)
     db.session.add(q1)
-
-    q2 = Quarto(nome="Banheiro", dimensoes="3x4 metros", casa=c1)
     db.session.add(q2)
-
     db.session.commit()
-    print(q1)
-    print(q2)
-    print()
-
-    print("*** TESTE com todos os dados")
-    print()
-    print(c1)
 
     # sem lista reversa
-    for q in db.session.query(Quarto).filter(Quarto.casa_id == c1.id).all():
+    for q in db.session.query(Comodo).filter(Comodo.casa_id == c1.id).all():
         print(q)
     print()
 
-    print("*** TESTE com todos os dados, via lista reversa")
-    print()
-    print(c1)
-
     # com lista reversa
-    for q in c1.quartos:
+    for q in c1.comodos:
         print(q)
     print()
 
     print("*** TESTE das mobílias")
     print()
-    m1 = Mobilia(nome = "Armário", funcao = "Guardar coisas", material = "Madeira", quarto=q1) 
-    db.session.add(m1)
-    db.session.commit()
-    print(m1)
-    print()
+    
+    m1 = Mobilia(nome = "armário", funcao = "guardar coisas", material = "madeira", comodo=q1)
+    m2 = Mobilia(nome = "espelho", funcao = "ajudar a se arrumar", material = "vidro polido", comodo = q2)
 
-    # não está em nenhum quarto
-    # versão 30/03 - Alice: adicionei "quarto=q2" que é o banheiro
-    m2 = Mobilia(nome = "Espelho", funcao = "Ajudar a se arrumar", material = "Vidro polido", quarto = q2)
+    db.session.add(m1)
     db.session.add(m2)
     db.session.commit()
-    print(m2)
-    print()
-
-    print("*** TESTE exibindo novamente todos os dados")
-    print()
-    print("*** TESTE com todos os dados CONECTADOS, via lista reversa")
-    print()
-    print("*** não vai exibir mobílias que não estão em quartos")
-    print()
-    print(c1)
-
-    print()
+    
     # quartos da casa, com lista reversa
-    for q in c1.quartos:
+    for q in c1.comodos:
         print(q)
         for m in q.mobilias:
             print(m)
